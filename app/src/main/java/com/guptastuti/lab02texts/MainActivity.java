79ef4dc7c7b38d988acc8c83af69c90b9aa03596
1738250 <stutigupta101@gmail.com>
2020-03-06 14:24:39 -0500

    took out two edittexts

--- app/src/main/java/com/guptastuti/lab02texts/MainActivity.java
+++ app/src/main/java/com/guptastuti/lab02texts/MainActivity.java
@@ -1,6 +1,8 @@
 package com.guptastuti.lab02texts;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,31 +12,41 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton1;
-    TextView displayText;
+    TextView displayText1;
+    EditText responseText;
+    TextView displayText2;
     Button submitButton2;
     int number = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Resources res = getResources();
+        final String[] planets_array = res.getStringArray(R.array.planets);
         submitButton1=findViewById(R.id.clickButton1);
         submitButton2=findViewById(R.id.clickButton2);
-        displayText=findViewById(R.id.textBox);
+        displayText1=findViewById(R.id.textBox1);
+        displayText2=findViewById(R.id.textBox2);
+        responseText=findViewById(R.id.responseEditText);
         submitButton1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                System.out.println("Hello mom");
                 Log.i("testButton1", "button1");
-//                displayText.setText(displayText.getText()+" "+responseText.getText());
-                number += 1;
-                displayText.setText("" + number);
+                displayText1.setText(responseText.getText());
             }
         });
         submitButton2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                System.out.println("Hello dad");
                 Log.i("testButton2", "button2");
+                displayText2.setText(planets_array[number]);
+                if (number == planets_array.length - 1)
+                {
+                    number = 0;
+                }
+                else {
+                    number += 1;
+                }
             }
         });
     }
